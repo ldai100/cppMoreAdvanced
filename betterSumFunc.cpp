09642fc997456fe3eb7fd21f1ad3f470e1ea6026
Li Dai <lidaivet@gmail.com>
2017-06-07 16:15:39 -0400

    variadic sum func completed

--- betterSumFunc.cpp
+++ betterSumFunc.cpp
@@ -1,3 +1,23 @@
-#inlcude <iostream>
+#include <iostream>
 
 using namespace std;
+
+template <typename T>
+T sum(T t)
+{
+  return t;
+}
+
+template <typename T, typename... Parameters>
+T sum(T first, Parameters... parameters)
+{
+  return first + sum(parameters...);
+}
+
+
+int main(){
+  cout << sum(1,2,3) << endl;
+  cout << sum(10,20) << endl;
+  cout << sum(1,1,1,1,1,1,1,1,1) << endl;
+  return 0;
+}
